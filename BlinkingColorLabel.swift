773a9d7a24ffce19ab59427820c54760ada6799b
Marcelo Sampaio <marcelosampaio1965@gmail.com>
2022-05-05 16:03:10 -0300

    checkpoint commit 🦖

--- BlinkingColorLabel.swift
+++ BlinkingColorLabel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+
+public class BlinkingColorLabel : UILabel {
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = .repeat
+        
+//        let options : UIViewAnimationOptions = .Repeat (|).rawValue .Autoreverse
+        UIView.animate(withDuration: 0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+ 
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
